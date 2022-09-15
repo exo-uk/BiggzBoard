0249ea6ccc360263353b59fdb6f8ba8992f776a3
Biggz <biggler@gmail.com>
2022-09-15 20:03:21 +0100

    Changed layout to "LAYOUT_planck"

--- qmk_firmware/keyboards/biggz/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/biggz/keymaps/default/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Ctl|Win│Alt│GUI│ < │       │ > │ ← │ ↓ │ ↑ │ → │
      * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
      */
-    [_LAYER0] = LAYOUT_planck_mit(
+    [_LAYER0] = LAYOUT_planck(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
     * `-----------------------------------------------------------------------------------'
     */
-    [_LAYER1] = LAYOUT_planck_mit(
+    [_LAYER1] = LAYOUT_planck(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
         KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
     * `-----------------------------------------------------------------------------------'
     */
-    [_LAYER2] = LAYOUT_planck_mit(
+    [_LAYER2] = LAYOUT_planck(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |      |      |      |      |      |             |      |      |      |      |      |
     * `-----------------------------------------------------------------------------------'
     */
-    [_LAYER3] = LAYOUT_planck_mit(
+    [_LAYER3] = LAYOUT_planck(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -102,19 +102,5 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(1, layer_state_cmp(state, _LAYER1));
     rgblight_set_layer_state(2, layer_state_cmp(state, _LAYER2));
     rgblight_set_layer_state(3, layer_state_cmp(state, _LAYER3));
-    /*switch (get_highest_layer(state)){
-        case _LAYER1:
-            rgblight_setrgb(0,255,0);
-            break;
-        case _LAYER2:
-            rgblight_setrgb(255,0,255);
-            break;
-        case _LAYER3:
-            rgblight_setrgb(255,128,0);
-            break;
-        default: // For other layers or default layer
-            rgblight_setrgb(0,0,255);
-            break;
-    }*/
     return state;
 }
--- qmk_firmware/keyboards/biggz/keymaps/rgb_test/keymap.c
+++ qmk_firmware/keyboards/biggz/keymaps/rgb_test/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │App│Ctl│Alt│GUI│ < │       │ > │ ← │ ↓ │ ↑ │ → │
      * └───┴───┴───┴───┴───┴───────┴───┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT_planck_mit(
+    [0] = LAYOUT_planck(
         RGB_TOG,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    RGB_VAI,    RGB_SAI,    RGB_HUI,
         RGB_MODE_PLAIN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    RGB_VAD,    RGB_SAD, RGB_HUD,
         RGB_MODE_KNIGHT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
